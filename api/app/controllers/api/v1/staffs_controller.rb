8a04ddbf3d77a287c13d264e3945d6ae26b8763e
Kenty-725 <kento00coding@gmail.com>
2025-09-18 21:50:13 +0900

    feat: 管理者仮登録処理追加

---
+++ api/app/controllers/api/v1/staffs_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class StaffsController < ApplicationController
+      def create
+        staff = Staff.new(staff_params)
+        # TODO: MVPでは問題ないが、スタッフ権限を持つスタッフを追加する際に調整する
+        staff.role = :admin # 管理者固定
+      
+        if staff.save
+          confirmation_url = "#{request.base_url}/api/v1/confirm?token=#{staff.confirmation_token}"
+          Rails.logger.info "[CONFIRMATION] URL: #{confirmation_url}"
+          render json: { message: "仮登録完了。ログに確認用URLを出力しました。" }, status: :created
+        else
+          render json: { errors: staff.errors.full_messages }, status: :unprocessable_entity
+        end
+      end      
+
+      private
+
+      def staff_params
+        params.require(:staff).permit(:name, :email, :password, :password_confirmation)
+      end
+    end
+  end
+end
