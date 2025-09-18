b2d450ff1df656ecbc4a5d244b0d312c7d7b7ae8
Kenty-725 <kento00coding@gmail.com>
2025-09-18 20:45:07 +0900

    feat: 認証周りで必要な設定

--- api/config/initializers/session_store.rb
+++ api/config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 Rails.application.config.session_store :cookie_store,
   key: '_sauna_app_session',
-  # In development, Lax avoids Chrome's SameSite=None+Secure requirement
-  same_site: Rails.env.production? ? :none : :lax,
+  same_site: :none,
   secure: Rails.env.production?
