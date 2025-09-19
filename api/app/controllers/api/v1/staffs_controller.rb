711dafedbb7de0dfba6c5bbf501f41ef0a24de59
Kenty-725 <kento00coding@gmail.com>
2025-09-19 21:38:03 +0900

    feat: 軽微な調整

--- api/app/controllers/api/v1/staffs_controller.rb
+++ api/app/controllers/api/v1/staffs_controller.rb
@@ -7,8 +7,9 @@ module Api
         
         if staff.persisted?
           confirmation_url = staff.generate_confirmation_url(request.base_url)
+          # TODO: メール送信の実装が必要でありその時に調整する
           Rails.logger.info "[CONFIRMATION] URL: #{confirmation_url}"
-          render json: { message: "仮登録完了。ログに確認用URLを出力しました。" }, status: :created
+          render json: { message: "施設管理者アカウント仮登録完了。ログに確認用URLを出力しました。" }, status: :created
         else
           render_validation_errors(staff)
         end
