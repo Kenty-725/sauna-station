df8c1f74335060fcf1c12befdfd11688cdf50217
Kenty-725 <kento00coding@gmail.com>
2025-09-21 14:50:22 +0900

    feat: オンボーディングページ作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OnboardingPage from "./pages/OnboardingPage";
-import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
@@ -8,8 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/staff/login" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
     </Router>
